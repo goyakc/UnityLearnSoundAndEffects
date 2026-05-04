0cbd6c7bb46cb3c061c9c880001e2cd11bc399bf
goyakc <goyakc@roverkids.org>
2026-05-04 14:08:42 -0400

    Version1.1-2

---
+++ JumpForce/JumpForce/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatDelay = 2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
